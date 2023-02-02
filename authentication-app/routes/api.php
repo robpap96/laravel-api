e06301b1ea097de83ef6ce1d483583de3442f678
robpap96 <robertopapagni1996@outlook.it>
2023-02-02 18:32:55 +0100

    milestone 2

--- authentication-app/routes/api.php
+++ authentication-app/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::get('test', function(){
+//     return [
+//         'name' => 'rob',
+//         'surname' => 'pap',
+//     ];
+// });
+
+Route::get('projects', [ProjectController::class, 'index']);

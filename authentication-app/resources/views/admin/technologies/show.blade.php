14ed2206886876a6b33d3229e63b0ff7b9d392bc
robpap96 <robertopapagni1996@outlook.it>
2023-02-07 11:56:48 +0100

    fixate show

--- authentication-app/resources/views/admin/technologies/show.blade.php
+++ authentication-app/resources/views/admin/technologies/show.blade.php
@@ -5,15 +5,16 @@
         <div class="py-4">
             <h4><a href="{{route('admin.technologies.index')}}">Torna alla lista</a></h4>
             <h1>{{$technology->name}}</h1>
-            @if($type->projects->isNotEmpty())
+            @if($technology->projects->isNotEmpty())
                 <h3>Post associati:</h3>
                 <ul>
                     @foreach ($technology->projects as $project)
-                        <li><a href="{{route('admin.projects.show', $project)}}">{{project->name}}</a></li>    
+                        <li><a href="{{route('admin.projects.show', $project)}}">{{$project->name}}</a></li>    
                     @endforeach
                 </ul>
             @else
-                <h3> Nessun progetto associato</h3>    
+                <h3> Nessun progetto associato</h3> 
+            @endif  
         </div>
     </div>
 @endsection

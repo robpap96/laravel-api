98474e874de399dbb916bfc03ca742c1a63e68c8
robpap96 <robertopapagni1996@outlook.it>
2023-02-07 17:51:34 +0100

    aggiunta api

--- authentication-app/routes/api.php
+++ authentication-app/routes/api.php
@@ -25,3 +25,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('projects', [ProjectController::class, 'index']);
 
 Route::get('projects/{slug}', [ProjectController::class, 'show']);
+
+Route::post('comments/{project_id}', [CommentController::class, 'store']);
